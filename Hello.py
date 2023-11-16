ef603a04138d02b3c3dedfa5df9b9e883c9c9c70
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2023-11-16 11:15:24 +0000

    new

--- Hello.py
+++ Hello.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-
+from st_pages import Page, show_pages, add_page_title
 
 
 
@@ -9,5 +9,24 @@ st.set_page_config(
         page_icon="👋",
     )
 
+with open("c.css") as f:
+    st.markdown(f"<style> {f.read()} </style>", unsafe_allow_html=True)
+
+  
 st.write("# هتل ساحل طلایی 👋")
 
+
+
+show_pages(
+    
+    [
+        
+        Page("app.py", "صفحه اصلی", "🏠"),
+        Page("page/game.py", "ساحل", "🎮"),
+        Page("page/application.py", "رستوران", "📳"),
+        Page("page/arz.py", "اتاق ها", "₿"),
+        Page("page/rahnama.py", "کافی شاب", "🚦"),
+        Page("page/rahnama.py", "مراسمات", "🚦"),
+    ]
+    
+)
