5701a33ddcaa06ebf8297eac20fd0b8e1445c5ab
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2023-11-17 11:10:09 +0000

    n

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from st_pages import Page, show_pages, add_page_title
-
+from annotated_text import annotated_text
 
 
 st.set_page_config(
@@ -12,8 +12,25 @@ st.set_page_config(
 with open("c.css") as f:
     st.markdown(f"<style> {f.read()} </style>", unsafe_allow_html=True)
 
+st.snow()
 st.image("logo.png")
-st.write("# هتل ساحل طلایی ")
+
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.header("هتل ساحل طلایی قشم")
+        
+annotated_text(
+    "هتل ",
+    ("طلایی", "ساحل"),
+    ( "قشم"),
+)
+
+with col2:
+    st.image("https://www.eghamat24.com/app/public/new_images/600x460/Qeshm-SahelTalaee-55.jpg")
+
+
 
 
 
