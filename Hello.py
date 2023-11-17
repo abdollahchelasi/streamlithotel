17a8ec6e56fd14ae3fb355d4900096e397d4b81a
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2023-11-17 10:34:14 +0000

    new

--- Hello.py
+++ Hello.py
@@ -6,14 +6,15 @@ from st_pages import Page, show_pages, add_page_title
 
 st.set_page_config(
         page_title="هتل ساحل طلایی",
-        page_icon="👋",
+        page_icon="logo.png",
     )
 
 with open("c.css") as f:
     st.markdown(f"<style> {f.read()} </style>", unsafe_allow_html=True)
 
-  
-st.write("# هتل ساحل طلایی 👋")
+st.image("logo.png")
+st.write("# هتل ساحل طلایی ")
+
 
 
 
@@ -22,11 +23,11 @@ show_pages(
     [
         
         Page("Hello.py", "صفحه اصلی", "🏠"),
-        Page("page/sahel.py", "ساحل", "🎮"),
-        Page("page/res.py", "رستوران", "📳"),
-        Page("page/room.py", "اتاق ها", "₿"),
-        Page("page/cafe.py", "کافی شاب", "🚦"),
-        Page("page/marasem.py", "مراسمات", "🚦"),
+        Page("page/sahel.py", "ساحل", "🌊"),
+        Page("page/res.py", "رستوران", "👨🏻‍🍳"),
+        Page("page/room.py", "اتاق ها", "⛪️"),
+        Page("page/cafe.py", "کافی شاب", "🍫"),
+        Page("page/marasem.py", "مراسمات", "🌈"),
     ]
     
 )
