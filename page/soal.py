2d7d6bff2a4e4c6a37f40a71c5876e933a18d951
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2023-11-21 10:57:40 +0330

    Update soal.py

--- page/soal.py
+++ page/soal.py
@@ -3,7 +3,7 @@ import streamlit as st
 with open("c.css") as f:
   st.markdown(f"<style> {f.read()} </style>", unsafe_allow_html=True)
 
-st.header("سوالات ساحل طلایی")
+st.header("❓ سوالات ❓")
 
 option = st.radio(
   """
