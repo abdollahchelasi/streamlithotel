cf8675b7f36a0f93ff396078574f5ae7a8b39352
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2023-11-22 08:40:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,8 @@ if selected == "جهت رزرو":
   c1,c2 = st.columns(2)
   phone_number = "+989025342900"
   with c1:
-     st.markdown(f'<a href="tel:{phone_number}">{phone_number}</a>', unsafe_allow_html=True)
+     # st.markdown(f'<a href="tel:{phone_number}">{phone_number}</a>', unsafe_allow_html=True)
+     st.markdown("[شمار ه تماس](tel:989025342900)")
      st.divider()
     
     
